98e184a1287cebcc1e7d4bc0928ac141cdef7473
Isaac <martinezhe@esat-alumni.com>
2022-10-19 22:05:59 +0200

    [+/*] Added jumping.
    Implemented basic ledge jumping.
    
    Needs to be revisited, as it gives some problems when landing or falling from tall grass.

--- Characters/Player/Player.cs
+++ Characters/Player/Player.cs
@@ -32,17 +32,20 @@ public class Player : KinematicBody2D
     //Editor Variables.
     [Export]
     public float                                 WalkSpeed = 4.0f;
+    public float                                 JumpSpeed = 4.0f;
 
     //Member Variables.
     private Vector2                             _initialPosition;
     private Vector2                             _inputDirection;
     private float                               _percentMovedToNextTile = 0.0f; //From 0 to 1.
     private bool                                _isMoving = false;
-    private AnimationTree                       _AnimTree;
-    private AnimationNodeStateMachinePlayback   _AnimState;
-    private PlayerState                         _PlayerState = PlayerState.PlayerState_Idle;
-    private FacingDirection                     _FacingDirection = FacingDirection.FacingDirection_Down;
-    private RayCast2D                           _Ray;
+    private AnimationTree                       _animTree;
+    private AnimationNodeStateMachinePlayback   _animState;
+    private PlayerState                         _playerState = PlayerState.PlayerState_Idle;
+    private FacingDirection                     _facingDirection = FacingDirection.FacingDirection_Down;
+    private RayCast2D                           _blockingRay;
+    private RayCast2D                           _jumpingRay;
+    private bool                                _isJumping = false;
 
     
 
@@ -63,26 +66,26 @@ public class Player : KinematicBody2D
         if(_inputDirection != Vector2.Zero)
         {
             //Update the Blend position with the input direction.
-            _AnimTree.Set("parameters/Idle/blend_position", _inputDirection);
-            _AnimTree.Set("parameters/Walk/blend_position", _inputDirection);
-            _AnimTree.Set("parameters/Turn/blend_position", _inputDirection);
+            _animTree.Set("parameters/Idle/blend_position", _inputDirection);
+            _animTree.Set("parameters/Walk/blend_position", _inputDirection);
+            _animTree.Set("parameters/Turn/blend_position", _inputDirection);
 
             //Update the player state based on the action performed.
             if(NeedToTurn())
             {
-                _PlayerState = PlayerState.PlayerState_Turning;
-                _AnimState.Travel("Turn");
+                _playerState = PlayerState.PlayerState_Turning;
+                _animState.Travel("Turn");
             }
             else
             {
-                _PlayerState = PlayerState.PlayerState_Walking;
+                _playerState = PlayerState.PlayerState_Walking;
                 _initialPosition = Position;
             }
         }
         else
         {
-            _PlayerState = PlayerState.PlayerState_Idle;
-            _AnimState.Travel("Idle");
+            _playerState = PlayerState.PlayerState_Idle;
+            _animState.Travel("Idle");
         }
     }
 
@@ -90,10 +93,36 @@ public class Player : KinematicBody2D
     {
         //Making sure the raycast is pointing to the direction the player is facing.
         Vector2 desired_step = _inputDirection * TILE_SIZE * 0.5f;
-        _Ray.CastTo = desired_step;
-        _Ray.ForceRaycastUpdate();
 
-        if(!_Ray.IsColliding())
+        _blockingRay.CastTo = desired_step;
+        _blockingRay.ForceRaycastUpdate();
+
+        _jumpingRay.CastTo = desired_step;
+        _jumpingRay.ForceRaycastUpdate();
+
+        Vector2 vec = new Vector2(0, 1);
+
+        //Check if the player is about to jump.
+        if ((_blockingRay.IsColliding() && _inputDirection == vec) || _isJumping)
+        {
+            _percentMovedToNextTile += JumpSpeed * delta;
+
+            if(_percentMovedToNextTile >= 2.0f)
+            {
+                Position = _initialPosition + (_inputDirection * TILE_SIZE * 2);
+                _percentMovedToNextTile = 0.0f;
+                _playerState = PlayerState.PlayerState_Idle;
+                _isJumping = false;
+            }
+            else
+            {
+                _isJumping = true;
+                float input = _inputDirection.y * TILE_SIZE * _percentMovedToNextTile;
+                Position = new Vector2(Position.x, _initialPosition.y + (-0.96f - 0.53f * input + 0.05f * (input * input)));
+            }
+        }
+        //If the player is not colliding.
+        else if (!_blockingRay.IsColliding())
         {
             //Update the percentage.
             _percentMovedToNextTile += WalkSpeed * delta;
@@ -103,7 +132,7 @@ public class Player : KinematicBody2D
             {
                 Position = _initialPosition + (TILE_SIZE * _inputDirection);
                 _percentMovedToNextTile = 0.0f;
-                _PlayerState = PlayerState.PlayerState_Idle;
+                _playerState = PlayerState.PlayerState_Idle;
             }
             //If the percentage is not at its maximum, interpolate the position with the percentage.
             else
@@ -127,7 +156,7 @@ public class Player : KinematicBody2D
         }
         else
         {
-            _PlayerState = PlayerState.PlayerState_Idle;
+            _playerState = PlayerState.PlayerState_Idle;
         }
     }
 
@@ -152,20 +181,20 @@ public class Player : KinematicBody2D
             new_facing_direction = FacingDirection.FacingDirection_Down;
         }
 
-        if(_FacingDirection != new_facing_direction)
+        if(_facingDirection != new_facing_direction)
         {
-            _FacingDirection = new_facing_direction;
+            _facingDirection = new_facing_direction;
             return true;
         }
 
-        _FacingDirection = new_facing_direction;
+        _facingDirection = new_facing_direction;
         return false;
     }
 
     public void FinishedTurning()
     {
         //When finished turning the player stays in idle.
-        _PlayerState = PlayerState.PlayerState_Idle;
+        _playerState = PlayerState.PlayerState_Idle;
     }
 
     //Start Function.
@@ -175,37 +204,38 @@ public class Player : KinematicBody2D
         _initialPosition = Position;
 
         //Initialize the animation objects.
-        _AnimTree = GetNode<AnimationTree>("AnimationTree");
-        _AnimState = (AnimationNodeStateMachinePlayback)_AnimTree.Get("parameters/playback");
+        _animTree = GetNode<AnimationTree>("AnimationTree");
+        _animState = (AnimationNodeStateMachinePlayback)_animTree.Get("parameters/playback");
 
-        _AnimTree.Active = true;
+        _animTree.Active = true;
 
-        //Initialize the RayCast Object.
-        _Ray = GetNode<RayCast2D>("RayCast2D");
+        //Initialize the RayCast Objects.
+        _blockingRay = GetNode<RayCast2D>("BlockingRayCast2D");
+        _jumpingRay = GetNode<RayCast2D>("JumpingRayCast2D");
     }
 
     //Called Every Frame.
     public override void _PhysicsProcess(float delta)
     {
-        if(_PlayerState == PlayerState.PlayerState_Turning)
+        if(_playerState == PlayerState.PlayerState_Turning)
         {
             return;
         }
-        else if(_PlayerState != PlayerState.PlayerState_Walking)
+        else if(_playerState != PlayerState.PlayerState_Walking)
         {
             ProcessPlayerInput();
         }
         //If the direction is not zero means the player has to move and perform animation.
         else if(_inputDirection != Vector2.Zero)
         {
-            _PlayerState = PlayerState.PlayerState_Walking;
-            _AnimState.Travel("Walk");
+            _playerState = PlayerState.PlayerState_Walking;
+            _animState.Travel("Walk");
             Move(delta);
         }
         else
         {
-            _PlayerState = PlayerState.PlayerState_Idle;
-            _AnimState.Travel("Idle");
+            _playerState = PlayerState.PlayerState_Idle;
+            _animState.Travel("Idle");
         }
     }
 }
